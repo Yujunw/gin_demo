8040d1a92538c7b692a19c164cb9c358a268b0be
Yujunw <691285799@qq.com>
2021-05-05 22:22:38 +0800

    使用restful api

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func sayHello(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"message":"hello golang",
+		"message": "hello golang",
 	})
 }
 
@@ -14,6 +17,28 @@ func main() {
 	// 指定用户使用GET请求访问/hello时，执行sayHello函数
 	r.GET("/hello", sayHello)
 
+	// 使用RESTful API
+	r.GET("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "GET",
+		})
+	})
+	r.POST("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "POST",
+		})
+	})
+	r.PUT("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "PUT",
+		})
+	})
+
+	r.DELETE("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "DELETE",
+		})
+	})
 	// 启动服务，默认在8080端口
 	r.Run(":9090")
 }
